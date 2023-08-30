7cb3c275288915ac1b7d0917081d6d6ede6de9af
Atropos <93047284+atroposmental@users.noreply.github.com>
2023-08-29 22:52:23 -0600

    Update SocialiteUser.php

--- src/Models/SocialiteUser.php
+++ src/Models/SocialiteUser.php
@@ -3,22 +3,31 @@
 namespace DutchCodingCompany\FilamentSocialite\Models;
 
 use DutchCodingCompany\FilamentSocialite\Facades\FilamentSocialite;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
 
-class SocialiteUser extends Model
-{
-    use HasFactory;
+class SocialiteUser extends Model {
+    use HasFactory,
+        HasTimestamps;
 
     protected $fillable = [
         'user_id',
         'provider',
         'provider_id',
+        'name',
+        'nickname',
+        'email',
+        'avatar_path',
+        'token',
+        'secret',
+        'refresh_token',
+        'expires_at',
     ];
 
-    public function user(): BelongsTo
-    {
+    public function user(): BelongsTo {
         return $this->belongsTo(FilamentSocialite::getUserModelClass());
     }
 }
